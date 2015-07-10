658ade86805af56b468e4c3ef48bd3339acd7e75
Miharbi <miharbihernandez@gmail.com>
2015-07-10 15:49:25 -0500

    folders, config, composer

---
+++ application/config/config.php
@@ -0,0 +1,10 @@
+<?php  if (! defined('BASEPATH')) {
+     exit('No direct script access allowed');
+ }
+
+$config['stripe']['mode']='test';
+$config['stripe']['sk_test'] = 'sk_test_hsR3m0antEs3Lr38809JT17h';
+$config['stripe']['pk_test'] = 'pk_test_NTJ76istO6ycY5NKPZ2PbeIg';
+$config['stripe']['sk_live'] = 'sk_live_Yc8lTTUt3v6Nm8txnJDRtBGg';
+$config['stripe']['pk_live'] = 'pk_live_hF5zJtZZI5LpH1YAc6sYw5zJ';
+$config['stripe']['currency'] = 'usd';
